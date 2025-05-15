fb07fc420fe3808ff1954d5cf2c886c790228455
Anna K <1415ks@ukr.net>
2025-05-15 21:27:20 +0300

    added useEffect та useDispatch

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Helmet } from "react-helmet";
+
+import { fetchContacts } from "../redux/contacts/operations";
 import ContactForm from "../components/ContactForm/ContactForm";
 import SearchBox from "../components/SearchBox/SearchBox";
 import ContactList from "../components/ContactList/ContactList";
 
 export default function ContactsPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <>
       <Helmet>
